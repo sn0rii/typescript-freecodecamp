2eaccb97d587f34bb390fe398a51e6ae78f1623e
Wojtek M <ojciecneo@gmail.com>
2023-05-31 08:28:25 +0200

    myObjects to js

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "Wojtek",
+    email: "wojtek@pl.pl",
+    isActive: true,
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "wojtek", isPaid: false });
+function createCourse() {
+    return { name: "react", price: 399 };
+}
