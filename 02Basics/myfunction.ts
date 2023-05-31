497f09e6012945bf6ce3ed2c5d688194158b7a04
Wojtek M <ojciecneo@gmail.com>
2023-05-31 07:29:00 +0200

    Function in ts part1

---
+++ 02Basics/myfunction.ts
@@ -0,0 +1,19 @@
+function addTwo(num: number) {
+  //   return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+let myValue = addTwo(5);
+getUpper("hello");
+
+signUpUser("wojtek", "wojtek@pl.pl", false);
+loginUser("h", "w@2.pl");
+
+export {};
