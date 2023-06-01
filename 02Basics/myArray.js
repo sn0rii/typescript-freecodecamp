d1b0fc58456ce7368c73155b2d33d1e719935715
Wojtek M <ojciecneo@gmail.com>
2023-06-01 06:34:16 +0200

    Arrays in TS

---
+++ 02Basics/myArray.js
@@ -0,0 +1,8 @@
+var superHeros = [];
+// const heroPower:number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [[255, 255, 255], []];
+allUsers.push({ name: "", isActive: true });
+superHeros.push("spiderman");
+heroPower.push(2);
---
+++ 02Basics/myArray.ts
@@ -0,0 +1,17 @@
+const superHeros: string[] = [];
+// const heroPower:number[] = []
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+allUsers.push({ name: "", isActive: true });
+
+superHeros.push("spiderman");
+heroPower.push(2);
