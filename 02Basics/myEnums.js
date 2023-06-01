839ad570c93ae6cdcfa202abc8e103b47e4defa0
Wojtek M <ojciecneo@gmail.com>
2023-06-01 08:26:08 +0200

    Enums in TS

---
+++ 02Basics/myEnums.js
@@ -0,0 +1,10 @@
+"use strict";
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = 3,
+//   WINDOW,
+//   FOURTH,
+// }
+// const wmSeat = SeatChoice.AISLE;
+Object.defineProperty(exports, "__esModule", { value: true });
+var wmSeat = "aisle" /* SeatChoice.AISLE */;
---
+++ 02Basics/myEnums.ts
@@ -0,0 +1,17 @@
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = 3,
+//   WINDOW,
+//   FOURTH,
+// }
+// const wmSeat = SeatChoice.AISLE;
+
+const enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 3,
+  WINDOW,
+  FOURTH,
+}
+const wmSeat = SeatChoice.AISLE;
+
+export {};
