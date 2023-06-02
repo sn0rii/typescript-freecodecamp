f3488d325b0c9e15cc88b394804acb0207371b31
Wojtek M <ojciecneo@gmail.com>
2023-06-02 09:01:56 +0200

    Private/Public TS

--- 03PureTs/dist/index.js
+++ 03PureTs/dist/index.js
@@ -1,10 +1,20 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Pionki";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(email, name, userID) {
         this.email = email;
         this.name = name;
+        this.userID = userID;
+        this.city = "Pionki";
     }
 }
-const wojtek = new User("w@2.pl", "wojtek");
-wojtek.city = "Pionki";
+const wojtek = new User("w@2.pl", "wojtek", "kl45");
+// wojtek.name
+// wojtek.city = "Warszawa";
--- 03PureTs/src/index.ts
+++ 03PureTs/src/index.ts
@@ -1,12 +1,20 @@
-class User {
-  email: string;
-  name: string;
-  city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
-}
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Pionki";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
 
-const wojtek = new User("w@2.pl", "wojtek");
-wojtek.city = "Pionki";
+class User {
+  readonly city: string = "Pionki";
+  constructor(
+    public email: string,
+    public name: string,
+    private userID: string
+  ) {}
+}
+const wojtek = new User("w@2.pl", "wojtek", "kl45");
+// wojtek.name
+// wojtek.city = "Warszawa";
