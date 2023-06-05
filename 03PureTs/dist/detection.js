c7141da22d5c647442552c1a8f371a96bd58f91e
Wojtek M <ojciecneo@gmail.com>
2023-06-05 07:41:40 +0200

    instance of and Type Predicates

--- 03PureTs/dist/detection.js
+++ 03PureTs/dist/detection.js
@@ -17,3 +17,24 @@ function isAdminAccount(account) {
         return account.isAdmin;
     }
 }
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
--- 03PureTs/src/detection.ts
+++ 03PureTs/src/detection.ts
@@ -29,3 +29,28 @@ function isAdminAccount(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
