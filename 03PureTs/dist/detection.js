218c078adcef7949d65561f4ceb5e3cf368b8eed
Wojtek M <ojciecneo@gmail.com>
2023-06-05 07:55:34 +0200

    narrowing in ts

--- 03PureTs/dist/detection.js
+++ 03PureTs/dist/detection.js
@@ -38,3 +38,22 @@ function getFood(pet) {
         return "bird food";
     }
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
