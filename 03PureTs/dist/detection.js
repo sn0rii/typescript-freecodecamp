a429f5e513b579e0c2ee44568beaac2e8ed11b19
Wojtek M <ojciecneo@gmail.com>
2023-06-05 07:06:28 +0200

    type narrowing ts

---
+++ 03PureTs/dist/detection.js
@@ -0,0 +1,14 @@
+"use strict";
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("please provide Id");
+        return;
+    }
+    id.toLowerCase();
+}
