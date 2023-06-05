7f41add9e77ce8986ecf92127f3486bd9deb8375
Wojtek M <ojciecneo@gmail.com>
2023-06-05 07:28:20 +0200

    in operator in TS

--- 03PureTs/src/detection.ts
+++ 03PureTs/src/detection.ts
@@ -12,3 +12,20 @@ function provideId(id: string | null) {
   }
   id.toLowerCase();
 }
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
